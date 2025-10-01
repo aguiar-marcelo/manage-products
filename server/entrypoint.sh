a76f0bf4428ddb3b22893e6baa10d5b7d43c9ad2
Marcelo aguiar <marcelodev2002@gmail.com>
2025-10-01 16:58:30 -0300

    fix dockerfile.server and entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,16 @@
+#!/bin/sh
+
+echo "Waiting for database to be ready..."
+
+while ! nc -z db 3306; do
+  sleep 1
+done
+
+echo "Database is ready. Starting migrations."
+
 /usr/local/bin/python manage.py makemigrations
 
 /usr/local/bin/python manage.py migrate --no-input
 
+echo "Starting Django server..."
 exec /usr/local/bin/python manage.py runserver 0.0.0.0:8000
